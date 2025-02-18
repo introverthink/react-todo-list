cde834d938ac82a29898c36beb38c2cbadf389f9
introverthink <justmaha53@gmail.com>
2025-02-19 00:23:21 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+
+import "./App.css"
+
+function App() {
+
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTask = () => {
+    if (input.trim() !== "") {
+      setTasks([...tasks, { text: input, completed: false }]);
+      setInput("");
+    }
+  };
+
+  const toggleTask = (index) => {
+    setTasks(
+      tasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((task, i) => i !== index));
+  };  
+
+  return (
+    <>
+      <div>
+          <input
+            type="text"
+            placeholder="Add a new task..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={addTask}>
+            Add
+          </button>
+      </div>
+
+      <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <span
+                className={task.completed ? "task-crossed" : ""}
+                onClick={() => toggleTask(index)}
+              >
+                {task.text}
+              </span>
+              <button onClick={() => deleteTask(index)}>X</button>
+            </li>
+          ))}
+      </ul>
+    </>
+  )
+}
+
+export default App
